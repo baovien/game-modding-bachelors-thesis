3ad53e7ea3c0fa4d0e30fba830977c83f064d78a
Vien Bao Ngo <vienbn15@student.uia.no>
2018-03-10 00:54:26 +0100

    Add: Base game. Map, player and followMouse. BKB-6

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Vector2 moveInput;
+    private Rigidbody2D myRigidBody2D;
+    private static bool playerExists;
+    public float moveSpeed;
+
+    // Use this for initialization
+    void Start()
+    {
+        myRigidBody2D = GetComponent<Rigidbody2D>();
+
+        if (!playerExists)
+        {
+            playerExists = true;
+            DontDestroyOnLoad(transform.gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       
+        moveInput = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized;
+
+        if (moveInput != Vector2.zero)
+        {
+            myRigidBody2D.velocity = new Vector2(moveInput.x * moveSpeed, moveInput.y * moveSpeed);
+        }
+        else
+        {
+            myRigidBody2D.velocity = Vector2.zero;
+        }
+
+    }
+}
