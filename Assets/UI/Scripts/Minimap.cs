6fe92b1772372b49f93e635e2ae4982a48c80fec
Vien Bao Ngo <vienbn15@student.uia.no>
2018-03-14 23:35:06 +0100

    Add: Minimap. Buttons is placeholders. BKB-6

---
+++ Assets/UI/Scripts/Minimap.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Minimap : MonoBehaviour
+{
+
+	public Transform player;
+
+	private Camera camera;
+	
+	
+	
+	private void LateUpdate()
+	{
+		Vector3 newPos = player.position;
+		newPos.z = transform.position.z;
+		transform.position = newPos;
+	}
+
+	public void ZoomIn()
+	{
+		
+	}
+}
